a3e7a7a52894a5877d733c2c520c0978da1586ee
dkur <dkur@ciklum.com>
2022-12-01 20:52:55 +0100

    Change uri

--- public/javascripts/engine/requests.js
+++ public/javascripts/engine/requests.js
@@ -1,4 +1,4 @@
-let serverUrl = "https://sketch-guesser.herokuapp.com/"
+let serverUrl = "https://doodle-guesser.lm.r.appspot.com/"
 
 function getRequest(url) {
     return new Promise(function (resolve, reject) {
